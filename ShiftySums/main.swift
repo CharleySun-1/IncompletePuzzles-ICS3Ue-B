2ecdd99ffff32a0660bcd2772c963dd1079f5f03
CharleySun <Ysun@lcs.on.ca>
2021-02-02 15:09:26 -0500

    Applied extension in the shifty sums.

--- ShiftySums/main.swift
+++ ShiftySums/main.swift
@@ -12,8 +12,9 @@ import Foundation
 
 print("Shifty Sums")
 print("===========")
-let n = Int(readLine()!)!
-let k = Int(readLine()!)!
+
+let n = Int.collectInput(withPrompt: "Enter the number you want to start with: ", minimum: 0, maximum: nil)
+let k = Int.collectInput(withPrompt: "Enter how many times do you want to shift it: ", minimum: 0, maximum: nil)
 
 func shift(n: Int, k:Int) -> Int {
 
